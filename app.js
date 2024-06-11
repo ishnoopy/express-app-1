9f7d33dc0be3d21b2a4fe569c86f119aefd6f584
aldrin <aldringuasa@gmail.com>
2024-06-11 21:24:48 +0800

    Revert "Initial Commit"
    This reverts commit 99baebb6f2aa21e48c7b7629fb3ec9508b769149.

--- app.js
+++ app.js
@@ -2,17 +2,6 @@
 const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
 require('dotenv').config({ path: envFile });
 
-// Process-level exception handler
-process.on('uncaughtException', (err) => {
-  console.error('There was an uncaught error', err);
-  process.exit(1); //mandatory (as per the Node.js docs)
-});
-
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-  process.exit(1);
-});
-
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const profileRoutes = require('./routes/profileRoutes');
@@ -30,9 +19,6 @@ app.use((err, req, res, next) => {
   res.json({ message: err.message });
 })
 
-app.listen(8080, function() {
-  console.log('Server is running on port 8080')
+app.listen(3000, function() {
+  console.log('Server is running on port 3000')
 })
-
-console.log('ENV:', process.env.NODE_ENV)
-console.log('JWT_SECRET:', process.env.JWT_SECRET)
